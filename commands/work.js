87c9bf4f465635a8b6bd5602e2a3b4e0f73e087a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-14 13:05:31 -0400

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -10,6 +10,23 @@ for(let i = 0; i < cooldowns.length; i++)
 }
 if (!cooldown)
 {
+	const situationArray = ["The apple has fallen from the tree!", "Mr. Jenkison threw a grape in my garage!", "I need to use the bathroom"]
+const situationKeyword = ["grab apple", "throw orange", "fruit juice"]
+let situationchoose = Math.round(Math.random() * 2) + 1
+if (Math.round(Math.random() * 2) + 1 === 2) {
+    message.channel.send("Random event time woo!");
+    message.channel.send(`${situationArray[situationchoose]}`);
+    message.channel.send(`Type ${situationKeyword[situationchoose]}`);
+    if (message.content.includes($ {
+            situationKeyword[situationchoose]
+        })) {
+        for (let i = 0; i < currency.length; i++) {
+            if (currency[i] === message.author.id) {
+                let earn = Math.round(Math.random() * 19) + 1 currency[i + 1] += earn;
+	
+	
+	
+	
 	let options = ["orange", "orange", "banana", "banana"];
 	let choice = options[Math.floor(Math.random() * options.length)];
 	if (choice === "orange")
@@ -56,25 +73,7 @@ if (!cooldown)
 										}
 									}
 								}
-const situationArray= ["The apple has fallen from the tree!","Mr. Jenkison threw a grape in my garage!","I need to use the bathroom"]
-const situationKeyword=["grab apple","throw orange","fruit juice"]
-let situationchoose =  Math.round(Math.random() * 2) + 1
-if (Math.round(Math.random() * 2) + 1 === 2) {
-message.channel.send("Random event time woo!");
-message.channel.send(`${situationArray[situationchoose]}`);	
-message.channel.send(`Type ${situationKeyword[situationchoose]}`);
-if (message.content.includes(${situationKeyword[situationchoose]})) {
-for(let i = 0; i < currency.length; i++)
-{
-if (currency[i] === message.author.id) {
-let earn = Math.round(Math.random() * 19) + 1
-currency[i + 1]+=earn;																		
 
-
-}
-	
-	
-}
 								else
 								{
 									message.channel.send("Sorry, but I want my orange. Please try and find it again.");
