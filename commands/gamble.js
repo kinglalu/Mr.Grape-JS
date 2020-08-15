6f0a7d530b35bea4a6a5b0051c1de9fcf90911a7
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-15 19:28:43 -0400

    Update gamble.js

--- commands/gamble.js
+++ commands/gamble.js
@@ -1,46 +1,84 @@
 //Usage: `gamble <amount>`, 50/50 chance of doubling your bet or losing it
 let cooldown = false
-for(let i = 0; i < cooldowns.length; i++)
-{
-	if (cooldowns[i] === cmd + message.author.id)
-	{
-		message.channel.send("bro chill out and wait a bit");
-		cooldown = true;
-	}
+for (let i = 0; i < cooldowns.length; i++) {
+    if (cooldowns[i] === cmd + message.author.id) {
+        message.channel.send("bro chill out and wait a bit");
+        cooldown = true;
+    }
 }
-if (!cooldown)
-{
-	cooldowns.push(cmd + message.author.id);
-	cooldowns.push("c5");
+if (!cooldown) {
+    cooldowns.push(cmd + message.author.id);
+    cooldowns.push("c5");
+    if (!parseInt(args[1]) || parseInt(args[1]) < 1 || parseInt(args[1]) > parseInt(currency[currency.indexOf(message.author.id) + 1])) {
+        message.channel.send("thats not a valid number of stars to gamble");
+    } else {
+        let roll = Math.round(Math.random() * 5) + 1
+        let starsWon = parseInt(args[1])
+        const gambleEmbed = new Discord.MessageEmbed()
+            .setColor('#dd2de0')
+            .setTitle(message.author.username + `'s gambling table`)
+            .addFields({
+                name: '--------------',
+                value: 'ok, if you roll an even number you win, if you roll an odd number, you lose'
+            }, )
+            .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+            .setTimestamp()
+            .setFooter('Grape Gambling Club.');
 
-	if (!parseInt(args[1]) || parseInt(args[1]) < 1 || parseInt(args[1]) > parseInt(currency[currency.indexOf(message.author.id) + 1]))
-	{
-		message.channel.send("thats not a valid number of stars to gamble");
-	}
-	else
-	{
-		let roll = Math.round(Math.random() * 5) + 1
-		message.channel.send("ok, if you roll an even number you win, if you roll an odd number, you lose");
-		setTimeout(function()
-		{
-			message.channel.send("you rolled a...");
-			setTimeout(function()
-			{
-				message.channel.send(roll);
-				setTimeout(function()
-				{
-					if (roll / 2 === Math.floor(roll / 2))
-					{
-						message.channel.send("aw, i lost");
-						currency[currency.indexOf(message.author.id) + 1] = parseInt(currency[currency.indexOf(message.author.id) + 1]) + parseInt(args[1]);
-					}
-					else
-					{
-						message.channel.send("ha, i win!");
-						currency[currency.indexOf(message.author.id) + 1] -= parseInt(args[1]);
-					}
-				}, 1000)
-			}, 3000)
-		}, 2000)
-	}
+        message.channel.send(gambleEmbed).then(msg => {
+                msg.delete({
+                    timeout: 2500
+                })
+            })
+            .catch(console.error);
+
+        setTimeout(function() {
+
+            message.edit(gambleEmbed.addFields({
+                name: '--------------',
+                value: 'you rolled a...'
+            }, ))
+
+            message.channel.send(gambleEmbed).then(msg => {
+                    msg.delete({
+                        timeout: 2700
+                    })
+                })
+                .catch(console.error);
+            setTimeout(function() {
+                message.edit(gambleEmbed.addFields({
+                    name: '--------------',
+                    value: roll
+                }, ))
+
+                message.channel.send(gambleEmbed).then(msg => {
+                        msg.delete({
+                            timeout: 2500
+                        })
+                    })
+                    .catch(console.error);
+                setTimeout(function() {
+                    if (roll / 2 === Math.floor(roll / 2)) {
+
+                        message.edit(gambleEmbed.addFields({
+                            name: '--------------',
+                            value: 'Congrats, you get ' + `${parseInt(args[1])}` + " :stars:s"
+                        }, ))
+
+                        message.channel.send(gambleEmbed);
+                        currency[currency.indexOf(message.author.id) + 1] = parseInt(currency[currency.indexOf(message.author.id) + 1]) + parseInt(args[1]);
+                    } else {
+
+                        message.edit(gambleEmbed.addFields({
+                            name: '--------------',
+                            value: 'You lost...'
+                        }, ))
+                        message.channel.send(gambleEmbed);
+
+                        currency[currency.indexOf(message.author.id) + 1] -= parseInt(args[1]);
+                    }
+                }, 1100)
+            }, 3100)
+        }, 2100)
+    }
 }
