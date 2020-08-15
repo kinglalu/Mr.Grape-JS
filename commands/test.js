3868d246187d3ceb925a2639539179c9cc72460d
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-15 17:18:51 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -31,23 +31,27 @@ if (!cooldown) {
                 name: '--------------',
                 value: 'you rolled a...'
             }, )
+            message.channel.send(gambleEmbed);
             setTimeout(function() {
                 gambleEmbed.addFields({
                     name: '--------------',
                     value: roll
                 }, )
+                 message.channel.send(gambleEmbed);
                 setTimeout(function() {
                     if (roll / 2 === Math.floor(roll / 2)) {
                         gambleEmbed.addFields({
                             name: '--------------',
                             value: 'Congrats, you get ' + `${parseInt(args[1])}` + " :stars:s"
                         }, )
+                         message.channel.send(gambleEmbed);
                         currency[currency.indexOf(message.author.id) + 1] = parseInt(currency[currency.indexOf(message.author.id) + 1]) + parseInt(args[1]);
                     } else {
                         gambleEmbed.addFields({
                             name: '--------------',
                             value: 'You lost...'
                         }, )
+                         message.channel.send(gambleEmbed);
                         currency[currency.indexOf(message.author.id) + 1] -= parseInt(args[1]);
                     }
                 }, 1000)
