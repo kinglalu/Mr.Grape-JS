83b54fdbbe0fdbd6fbfb338e7f35a0b0e2148ebe
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-16 15:18:03 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,62 +7,57 @@ for (let i = 0; i < cooldowns.length; i++) {
     }
 }
 if (!cooldown) {
-   //what work will you have?
+    //what work will you have?
     let workSituation = Math.floor(Math.random() * 0) + 1
-   //program embed for orange job 
-   
+    //program embed for orange job 
+
     const orangeJobEmbed = new Discord.MessageEmbed()
         .setColor('#dd2de0')
         .setTitle(message.author.username + `'s job`)
-        .addFields (
-            {name: 'Find that orange!', value: 'will you help me find my orange?/nit fell in a bush full of bananas over there, but i could not find it./nPlease go there and find my orange. ' },
-            )
+        .addFields({
+            name: 'Find that orange!',
+            value: 'will you help me find my orange?/nit fell in a bush full of bananas over there, but i could not find it./nPlease go there and find my orange. '
+        }, )
         .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
         .setTimestamp()
         .setFooter('Grape Jobs Inc.');
- if (workSituation === 1) {
-     message.channel.send(orangeJobEmbed);
-      let options = ["orange", "orange", "banana", "banana"];
-    let choice = options[Math.floor(Math.random() * options.length)];
-    if (choice === "orange")
+    if (workSituation === 1) {
+        message.channel.send(orangeJobEmbed);
+        let options = ["orange", "orange", "banana", "banana"];
+        let choice = options[Math.floor(Math.random() * options.length)];
+        if (choice === "orange") {
+            for (let i = 0; i < currency.length; i++) {
+                if (currency[i] === message.author.id) {
+                    let earn = Math.round(Math.random() * 19) + 1
+                    currency[i + 1] += earn;
+                    message.edit(orangeJobEmbed.addFields({
+                        name: 'You got ' + earn + 'gold :star:s!',
+                        value: ''
+                    }, ))
 
-    {
+                    message.channel.send(orangeJobEmbed).then(msg => {
+                        msg.delete({
+                            timeout: 500
+                        })
+                    })
+                }
+            }
+        }
         cooldowns.push(cmd + message.author.id);
         cooldowns.push("c77");
     } else {
-        cooldowns.push(cmd + message.author.id);
-        cooldowns.push("c17");
+        message.edit(jobEmbed.addFields({
+            name: 'Sorry, you did not find it. please try to find my orange later.',
+            value: ''
+        }, ))
+
+        message.channel.send(jobEmbed).then(msg => {
+            msg.delete({
+                timeout: 500
+            })
+        })
     }
-                                if (choice === "orange") {
-                                    for (let i = 0; i < currency.length; i++) {
-                                        if (currency[i] === message.author.id) {
-                                            let earn = Math.round(Math.random() * 19) + 1
-                                            currency[i + 1] += earn;
-                                            message.edit(orangeJobEmbed.addFields({
-                                                name: 'You got ' + earn + 'gold :star:s!',
-                                                value: ''
-                                            }, ))
 
-                                            message.channel.send(orangeJobEmbed).then(msg => {
-                                                msg.delete({
-                                                    timeout: 500
-                                                })
-                                            })
-                                        }
-                                    }
-                                } 
- }                              else {
-                                    //message.channel.send("Sorry, but I want my orange. Please try and find it again.");
-                                    message.edit(jobEmbed.addFields({
-                                        name: 'Sorry, please try to find my orange later.',
-                                        value: ''
-                                    }, ))
-
-                                    message.channel.send(jobEmbed).then(msg => {
-                                        msg.delete({
-                                            timeout: 500
-                                        })
-                                    })
-                                }                          
-
-
+    cooldowns.push(cmd + message.author.id);
+    cooldowns.push("c17");
+}
