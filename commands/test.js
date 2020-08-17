5062379c76199dffb626b7e5c4e3a736b4d06d03
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-17 16:51:18 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,52 +1,40 @@
 //Usage: `work`, get golden stars by helping people, 60s cooldown
 let cooldown = false
-for(let i = 0; i < cooldowns.length; i++)
-{
-	if (cooldowns[i] === cmd + message.author.id)
-	{
-		message.channel.send("Sorry, i dont have any jobs for you");
-		cooldown = true;
-	}
+for (let i = 0; i < cooldowns.length; i++) {
+    if (cooldowns[i] === cmd + message.author.id) {
+        message.channel.send("Sorry, i dont have any jobs for you");
+        cooldown = true;
+    }
 }
-let random = Math.floor(Math.random() * 19) + 1;
-function giveMoney() 
-{    
- for(let i = 0; i < currency.length; i++)
-    {
-        if (currency[i] === message.author.id)
-        {
-            currency[i + 1] = parseInt(currency[i + 1]) + random;
-        }
-    }   
-}
-if (!cooldown)
-{
-    let workSituation = Math.floor(Math.random() * 1) + 1;
-    if (workSituation === 1) {
-	let options = ["orange", "orange", "banana", "banana"];
-	let choice = options[Math.floor(Math.random() * options.length)];
-	if (choice === "orange")
-	{
-		giveMoney();
-        const dailystarEmbed = new Discord.MessageEmbed()
+if (!cooldown) {
+    let options = ["orange", "orange", "banana", "banana"];
+    let choice = options[Math.floor(Math.random() * options.length)];
+    if (choice === "orange") {
+        cooldowns.push(cmd + message.author.id);
+        cooldowns.push("c77");
+    } else {
+        cooldowns.push(cmd + message.author.id);
+        cooldowns.push("c17");
+    }
+
+    if (choice === "orange") {
+        for (let i = 0; i < currency.length; i++) {
+            if (currency[i] === message.author.id) {
+                let earn = Math.round(Math.random() * 19) + 1
+                currency[i + 1] += earn;
+		      const dailystarEmbed = new Discord.MessageEmbed()
 					.setColor('#dd2de0')
 					.setTitle(message.author.username + `'s daily reward`)
 					.addFields(
-						{ name: 'Daily Reward', value: 'here is ' + ` ${this.random} ` + ' :star:s' },
+						{ name: 'Daily Reward', value: 'here is ' + ` ${earn} ` + ' :star:s' },
 					)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
 					.setFooter('Grape Bank Inc.');
 
 		message.channel.send(dailystarEmbed);
-        cooldowns.push(cmd + message.author.id);
-		cooldowns.push("c77");
-	}
-	else
-	{
-		message.channel.send('no monies for you')
-        cooldowns.push(cmd + message.author.id);
-		cooldowns.push("c17");
-	}
+            }
+        }
+    } else {
+        message.channel.send("Sorry, but I want my orange. Please try and find it again.");
     }
-    else if (workSituation === 2) {message.channel.send('idk some other work')}
