3f3e10ec3449ebdb95e6511aa6d7822c2340c93d
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-17 16:56:56 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,6 +7,7 @@ for (let i = 0; i < cooldowns.length; i++) {
     }
 }
 if (!cooldown) {
+	let random = Math.floor(Math.random() * 19) + 1;
     let options = ["orange", "orange", "banana", "banana"];
     let choice = options[Math.floor(Math.random() * options.length)];
     if (choice === "orange") {
@@ -18,23 +19,25 @@ if (!cooldown) {
     }
 
     if (choice === "orange") {
-        for (let i = 0; i < currency.length; i++) {
-            if (currency[i] === message.author.id) {
-                let earn = Math.round(Math.random() * 19) + 1
-                currency[i + 1] += earn;
-		      const dailystarEmbed = new Discord.MessageEmbed()
+	    const dailystarEmbed = new Discord.MessageEmbed()
 					.setColor('#dd2de0')
 					.setTitle(message.author.username + `'s daily reward`)
 					.addFields(
-						{ name: 'Daily Reward', value: 'here is ' },
+						{ name: 'Daily Reward', value: 'here is ' + ` ${random} ` + ' :star:s' },
 					)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
 					.setFooter('Grape Bank Inc.');
 
 		message.channel.send(dailystarEmbed);
-            }
+         for(let i = 0; i < currency.length; i++)
+    {
+        if (currency[i] === message.author.id)
+        {
+            currency[i + 1] = parseInt(currency[i + 1]) + random;
         }
+    }
     } else {
         message.channel.send("Sorry, but I want my orange. Please try and find it again.");
     }
+}
