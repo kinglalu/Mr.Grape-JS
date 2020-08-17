b051a2954c09aa386c7c85ffeaba9460cba36304
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-16 20:36:08 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 //Usage: `work`, get golden stars by helping people, 60s cooldown
-let cooldown = false
+let cooldown = true
 for (let i = 0; i < cooldowns.length; i++) {
     if (cooldowns[i] === cmd + message.author.id) {
         message.channel.send("Sorry, i dont have any jobs for you");
@@ -16,12 +16,16 @@ if (!cooldown) {
          
         let options = ["orange", "orange", "banana", "banana"];
         let choice = options[Math.floor(Math.random() * options.length)];
+        let earn = Math.round(Math.random() * 19) + 1
         if (choice === "orange") {
             for (let i = 0; i < currency.length; i++) {
                 if (currency[i] === message.author.id) {
-                    let earn = Math.round(Math.random() * 19) + 1
                     currency[i + 1] += earn;
-         const orangeWin = new Discord.MessageEmbed()
+         
+                    
+                }
+            }
+            const orangeWin = new Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s job`)
             .addFields({name: 'Find that orange!', value: 'will you help me find my orange?\nit fell in a bush full of bananas over there, but i could not find it.\nPlease go there and find my orange. '}, 
@@ -32,9 +36,6 @@ if (!cooldown) {
             .setFooter('Grape Jobs Inc.');
 
                     message.channel.send(orangeWin)
-                    
-                }
-            }
         }
         cooldowns.push(cmd + message.author.id);
         cooldowns.push("c77");
