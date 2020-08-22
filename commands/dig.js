a7e7da4c8d8b0b8fed15cb741b8c365dbdd79963
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-22 17:30:33 -0400

    Update and rename test.js to dig.js

---
+++ commands/dig.js
@@ -0,0 +1,26 @@
+//Usage: `work`, get golden stars by helping people, 60s cooldown
+let cooldown = false
+for (let i = 0; i < cooldowns.length; i++) {
+    if (cooldowns[i] === cmd + message.author.id) {
+        message.channel.send("Sorry, i dont have any jobs for you");
+        cooldown = true;
+    }
+}
+if (!cooldown) {
+	let earn = let earn = Math.round(Math.random() * 6) + 1;
+const mine = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(message.author.username + `'s mine`)
+					.addFields (
+						{name:'You dug up '+earn+' :star:s'  , value: '_'},
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Enterprises');
+	for (let i = 0; i < currency.length; i++) {
+            if (currency[i] === message.author.id) {           
+                currency[i + 1] += earn;		     
+            }
+        }
+
+}
