cac46b4816a3df45affe432c07c6d0946d74e6d1
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-22 13:57:55 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -56,9 +56,55 @@ if (!cooldown) {
 	else {message.channel.send('Uh oh, some fruit juice spilled. (Unknown error)')
 }
 	}
-	
+	function mango () {let choice = Math.floor(Math.random() * (100 - 1) ) + 1;
+	let earn = Math.round(Math.random() * 7) + 1;
+    if (choice === "orange") {
+        cooldowns.push(cmd + message.author.id);
+        cooldowns.push("c77");
+    } else {
+        cooldowns.push(cmd + message.author.id);
+        cooldowns.push("c17");
+    }
+
+    if (choice % 2 === 0) {
+	     const mangoJobGood = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(message.author.username + `'s job`)
+					.addFields (
+						{name: 'Help Mr.Grape catch his mango!' , value: 'I am trying to catch a flying mango, but it keeps disappearing.\nSo will you catch him and bring him to me?'},
+						{name: 'Yay, you found my mango! Here, take '+earn+' :star:s!', value: '_'},
+						)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Enterprises');
+
+		message.channel.send(mangoJobGood);
+        for (let i = 0; i < currency.length; i++) {
+            if (currency[i] === message.author.id) {           
+                currency[i + 1] += earn;		     
+            }
+        }
+    } else if (choice % 2 === 1 || choice === 1) {
+         const orangeJobBad = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(message.author.username + `'s job`)
+					.addFields (
+          {name: 'Help Mr.Grape catch his mango!' , value: 'I amm trying to catch a flying mango, but it keeps disappearing.\nSo will you catch him and bring him to me?'},
+						{name: 'You didnt find my mango? Too bad. Try again next time.' , value: '_'},
+						
+						)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Enterprises');
+	    message.channel.send(orangeJobBad);
+	    
+    }
+	else {message.channel.send('Uh oh, some fruit juice spilled. (Unknown error)')
+}
+}
+}
 	if (chooseWork === 1) {orange();}
-	else if (chooseWork === 2) {message.channel.send('idk some mango thing')}
+	else if (chooseWork === 2) {mango();}
 	else {message.channel.send('Uh oh, some fruit juice spilled. (Unknown error)')}
 	
 }
