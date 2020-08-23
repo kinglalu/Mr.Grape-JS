e88db8028aa70c8b26e3bda8a4f2c07ec29dc3f8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-23 09:43:55 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -18,21 +18,31 @@ for (let i = 0; i < cooldowns.length; i++) {
     }
 }
 if (!cooldown) {
-let wordsOfWisdom = ['Better not tell you now.', ']
-
-
-}
- 'Don’t count on it.',
+let wordsOfWisdom = ['Better not tell you now.', 'Don’t count on it.',
  'It is certain.',
  'It is decidedly so.',
  'Most likely.',
 ' My reply is no.',
  'My sources say no.',
  'Outlook not so good.',
- 'Outlook good.'
- 'Signs point to yes.'
- 'Very doubtful.'
- 'Without a doubt.'
- 'Yes.'
- 'Yes – definitely.'
- 'You may rely on it.'
+ 'Outlook good.',
+ 'Signs point to yes.',
+ 'Very doubtful.',
+ 'Without a doubt.',
+ 'Yes.',
+ 'Yes – definitely.',
+ 'You may rely on it.'];
+ let random =  Math.round(Math.random() * wordsOfWisdom.length);
+        const ballEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('Info')
+					.addFields(
+						{ name: reject[random], value: '(Cooldown)'}
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Fortune-Telling');
+        message.channel.send(ballEmbed);
+
+}
+ 
