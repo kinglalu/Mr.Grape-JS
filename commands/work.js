e73a8b323f48004e205a6f1565c5fde26450c2dc
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-23 12:09:00 -0400

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -2,7 +2,15 @@
 let cooldown = false
 for (let i = 0; i < cooldowns.length; i++) {
     if (cooldowns[i] === cmd + message.author.id) {
-        message.channel.send("Sorry, i dont have any jobs for you");
+        const balnoEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle("Sorry, I don't have any jobs for you.")
+					
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					
+
+				message.channel.send(balnoEmbed);
         cooldown = true;
     }
 }
