c8fee3f12a0b6944e41ca2023ed39705b6bccbc9
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-23 03:51:40 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,7 +2,18 @@
 let cooldown = false
 for (let i = 0; i < cooldowns.length; i++) {
     if (cooldowns[i] === cmd + message.author.id) {
-        message.channel.send("Ask again later")
+        let reject = ['Ask again later','Cannot predict now.','Concentrate and ask again','Reply hazy, try again.'];
+        let random =  Math.round(Math.random() * reject.length);
+        const rejectEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('Info')
+					.addFields(
+						{ name: reject[random], value: '(Cooldown)'}
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Fortune-Telling');
+        message.channel.send(rejectEmbed);
         cooldown = true;
     }
 }
@@ -11,18 +22,17 @@ let wordsOfWisdom = ['Better not tell you now.', ']
 
 
 }
- 'Don’t count on it.'
- 'It is certain.'
- 'It is decidedly so.'
- 'Most likely.'
-' My reply is no.'
- 'My sources say no.'
- Outlook not so good.'
- Outlook good.'
- Reply hazy, try again.'
- Signs point to yes.'
- Very doubtful.'
- Without a doubt.'
- Yes.'
- Yes – definitely.'
- You may rely on it.'
+ 'Don’t count on it.',
+ 'It is certain.',
+ 'It is decidedly so.',
+ 'Most likely.',
+' My reply is no.',
+ 'My sources say no.',
+ 'Outlook not so good.',
+ 'Outlook good.'
+ 'Signs point to yes.'
+ 'Very doubtful.'
+ 'Without a doubt.'
+ 'Yes.'
+ 'Yes – definitely.'
+ 'You may rely on it.'
