3de126ae218bdb50eb7637ddcbca42805e1dfeda
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-06 17:21:08 -0400

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -48,13 +48,16 @@ if (!cooldown) {
 
     } else {
     async function Auth() {           
+           
             if (await users.get(message.author.id) === undefined) {
-              
-                
-                  users.set(message.author.id, 0);
-                  users.set(message.author.id, 0);
+                for(let i = 0; i < 10; i++) {  
+                users.set(message.author.id, 0);
+                    if(await users.get(message.author.id) === 0) {
+                        break;
+                    }
               }
            }
+    }
         
 
       
