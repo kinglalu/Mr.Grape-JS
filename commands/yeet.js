bc84edae7b8922b758bf4891318688bc59c7a54f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-06 14:45:30 -0400

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -1,9 +1,72 @@
-async function display() {  
-if (await users.get(message.author.id) === undefined) {
-  users.set(message.author.id, 0);
-}
-message.channel.send(`${await users.get(message.author.id)}`);
+//Usage: `bal <user>`, returns the number of stars a user has
+let cooldown = false
+for(let i = 0; i < cooldowns.length; i++)
+{
+	if (cooldowns[i] === cmd + message.author.id)
+	{
+		const balnoEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('bro chill out and wait a bit')
+					
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					
 
+				message.channel.send(balnoEmbed);
+		cooldown = true;
+	}
+}
+if (!cooldown)
+{
+  
+	cooldowns.push(cmd + message.author.id);
+	cooldowns.push("c5");
+
+	if (target)
+	{
+       async function displayTarget() {  
+        if (await users.get(target.id) === undefined) {
+        users.set(target.id, 0);
+        }}
+	        displayTarget();
+				const balEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(target.displayName + `'s balance`)
+					.addFields(
+						{ name: 'Balance', value: target.displayName + ' has ' + ` ${await users.get(target.id)} ` + ' :star:' },
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Bank Inc.');
+
+				message.channel.send(balEmbed);
+			
+	}
+	else
+	{
+		async function displayTarget() {  
+        if (await users.get(message.author.id) === undefined) {
+        users.set(message.author.id, 0);
+        }}
+	        displayTarget();
+				const balsoloEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(message.author.username + `'s balance`)
+					.addFields(
+						{ name: 'Balance', value:  'you have ' + ` ${await users.get(message.author.id)} ` + ' :star:' },
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Bank Inc.');
+
+				message.channel.send(balsoloEmbed);
+				
+				
+				
+				
+				
+				//message.channel.send(`you have \`${currency[i + 1]}\` golden star(s)`);
+
+	}
 }
 
-display();
