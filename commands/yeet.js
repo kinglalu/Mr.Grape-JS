21ada139cba4da1c4315524caab47e5eee58d36f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-06 14:30:57 -0400

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -1,5 +1,9 @@
-async function display() {
+async function display() {  
+if (await users.get(message.author.id) === 'undefined') {
+  users.set(message.author.id, 0);
+}
 message.channel.send(`${await users.get(message.author.id)}`);
+
 }
 
 display();
