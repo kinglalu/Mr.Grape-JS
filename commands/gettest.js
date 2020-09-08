5f9562f583f43a70db006fb26d0351337f3a131f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 18:51:31 -0400

    Update gettest.js

--- commands/gettest.js
+++ commands/gettest.js
@@ -5,5 +5,7 @@ message.channel.send(noicevar);
 let rand = Math.floor(Math.random()*9)+1;
 let anothervar = noicevar + rand;
 message.channel.send(anothervar)
+await users.set(message.author.id, anothervar);
+message.channel.send(`${await users.set(message.author.id, anothervar);}`)
 }
 noice();
