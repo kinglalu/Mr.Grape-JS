1ecff0ddd0b1b68e466ec9b6976257bf7bb7feac
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 03:43:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,6 +51,19 @@ bot.on("message", async(message) =>
 		target = message.guild.members.cache.find(member => member.id === args[1].replace("<@", "").replace(">", ""));
 	}
 	
+	function getUserFromMention(mention) {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
+}
 
 	if (commands.includes(`${cmd}.js`))
 	{
