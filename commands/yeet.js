c491599f4303ce17c1f5b40525a6a9844da9608b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 09:14:15 -0400

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -15,6 +15,76 @@ for (let i = 0; i < cooldowns.length; i++) {
     }
 }
 if (!cooldown) {
-lol();
+if (!target) {
+    async function bal() {
+          if (await users.get(message.author.id) === undefined) {
+                users.set(message.author.id, 0);
+                  const balsolooEmbed = new Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(message.author.username + `'s balance`)
+                .addFields({
+                    name: 'Balance',
+                    value: 'you have 0 :star:s'
+                }, )
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Bank Inc.');
+                message.channel.send(balsolooEmbed);
+            }
+            else {  
+                const balsoloEmbed = new Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(message.author.username + `'s balance`)
+                .addFields({
+                    name: 'Balance',
+                    value: 'you have '+`${await users.get(message.author.id)}`+' :star:s'
+                }, )
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Bank Inc.');
+            message.channel.send(balsoloEmbed);
+            }
+    }
+    bal();
+}
+    else {
+    async function balTarg() {
+          if (await users.get(target) === undefined) {
+                users.set(target, 0);
+                  const balsolooEmbed = new Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(target.displayName + `'s balance`)
+                .addFields({
+                    name: 'Balance',
+                    value: 'you have 0 :star:s'
+                }, )
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Bank Inc.');
+                message.channel.send(balsolooEmbed);
+            }
+            else {  
+                const balsoloEmbed = new Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(target.displayName + `'s balance`)
+                .addFields({
+                    name: 'Balance',
+                    value: 'you have '+`${await users.get(target)}`+' :star:s'
+                }, )
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Bank Inc.');
+            message.channel.send(balsoloEmbed);
+            }
+    
+    }
+    
+  balTarg();
+    
+    
+    
+    
+    }
+    
 }
 
