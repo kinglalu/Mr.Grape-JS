58201bf27bd25c59c9ca31cb4bdc9635f2e8b9d7
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 17:00:35 -0400

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -78,13 +78,17 @@ if (!cooldown) {
     
     }
 	
-	if (!target) {
-		  bal();
+	if (args[0]) {
+		const usery = getUserFromMention(args[0]);
+		if (!usery) {
+			return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
 		}
-	else {
-		balTarg();
+
+		return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
 	}
 
+	return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+}
     
 }
     
