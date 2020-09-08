f2dc809dd0dded6495744d19949be7df50f3e83c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 18:42:12 -0400

    Update gettest.js

--- commands/gettest.js
+++ commands/gettest.js
@@ -2,5 +2,7 @@ async function noice() {
 message.channel.send(`${await users.get(message.author.id)}`)
 let noicevar = await users.get(message.author.id)
 message.channel.send(noicevar);
+let anothervar = noicevar + 4;
+message.channel.send(anothervar)
 }
 noice();
