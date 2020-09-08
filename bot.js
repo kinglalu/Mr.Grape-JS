e3aedc400bad034f7fbe825bab6463b2173580f9
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-08 10:05:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,20 +50,8 @@ bot.on("message", async(message) =>
 	{
 		target = message.guild.members.cache.find(member => member.id === args[1].replace("<@", "").replace(">", ""));
 	}
-	
-function getUserFromMention(mention) {
-	// The id is the first and only match found by the RegEx.
-	const matches = mention.match(/^<@!?(\d+)>$/);
 
-	// If supplied variable was not a mention, matches will be null instead of an array.
-	if (!matches) return;
 
-	// However the first element in the matches array will be the entire mention, not just the ID,
-	// so use index 1.
-	const id = matches[1];
-
-	return client.users.cache.get(id);
-}
 	if (commands.includes(`${cmd}.js`))
 	{
 		eval(fs.readFileSync(`./commands/${cmd}.js`).toString());
