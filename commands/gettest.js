8c1607d8a95023499cd5d880cb469291e89bb535
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-09 09:32:23 -0400

    Update gettest.js

--- commands/gettest.js
+++ commands/gettest.js
@@ -10,3 +10,19 @@ await users.set(message.author.id, anothervar);
 message.channel.send(`${await users.get(message.author.id)}`+"same thing as b4")
 }
 noice();
+message.channel.send('Above it reference function');
+async function dolla(who, howmuch) {
+    message.channel.send(howmuch+"howmuchiadd");
+    let rightnow = await users.get(who);
+    message.channel.send(rightnow+"current$");
+    let moremoni = rightnow + howmuch;
+    await users.set(who, moremoni)
+  message.channel.send(`${await users.get(who)}`+"now")
+}
+  
+  dolla(message.author.id,4);
+  
+  
+  
+  
+  
