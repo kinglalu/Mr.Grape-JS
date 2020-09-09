6ec7c2d2dc91fee0dcd12adc96bec6169f5c8541
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-09 09:10:38 -0400

    Update gettest.js

--- commands/gettest.js
+++ commands/gettest.js
@@ -1,11 +1,12 @@
 async function noice() {
-message.channel.send(`${await users.get(message.author.id)}`)
+await users.set(message.author.id, 7);
+message.channel.send(`${await users.get(message.author.id)}`+"should print 7")
 let noicevar = await users.get(message.author.id)
-message.channel.send(noicevar);
+message.channel.send(noicevar+"should still print 7");
 let rand = Math.floor(Math.random()*9)+1;
 let anothervar = noicevar + rand;
-message.channel.send(anothervar)
+message.channel.send(anothervar+"7 plus a random number 1-10")
 await users.set(message.author.id, anothervar);
-message.channel.send(`${await users.get(message.author.id)}`)
+message.channel.send(`${await users.get(message.author.id)}`+"same thing as b4")
 }
 noice();
