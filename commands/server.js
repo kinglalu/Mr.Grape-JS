d6d14bc8ed70ecb0c2a0e417aae86c6c0f896691
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-09 10:42:39 -0400

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,15 @@
+//Usage: `server`, Returns some basic info about the server
+//message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+
+const serversoloEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('Server Info')
+					.addFields(
+						{ name: 'Server name:', value: `${message.guild.name}`},
+						{ name: 'Members:', value: `${message.guild.memberCount}`},	
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Databases');
+
+				message.channel.send(serversoloEmbed);
