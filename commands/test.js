273b9411b67c1b512206c7b0bb46ac0f4b6f5266
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-10 10:08:09 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,6 @@
 async function check() {
-	const a = await users.get(message.author.id);
+	let a = await users.get(message.author.id);
+	message.channel.send(a);
 	let argu = parseInt(args[2]);
 	let check = Math.sign(a - argu);
 	if (check === -1) {
