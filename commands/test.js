387f55f9f5d622124e1e694b84702acfb409495b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-10 09:47:32 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,8 @@
 async function check() {
 	let rightnow = await users.get(message.author.id);
-	let noice = rightnow + 1;
-	message.channel.send("asdf"+noice);
-	if (rightnow > parseInt(args[2])) {
+	let noice = rightnow + 0;
+	
+	if (noice > parseInt(args[2])) {
 		message.channel.send("treu");
 		return true;}
 	}
