70f5f9b8ed37495e60951a56d7a4d3fb3544c0ca
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-10 09:10:38 -0400

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -23,13 +23,15 @@ if (!cooldown)
 	async function getDolla() {
 	let rightnow = await users.get(message.author.id);
 	return rightnow;}
+	let check = getDolla;
+	message.channel.send("moni: "  + check);
 	let targets = message.mentions.members.first();
         let targetss = targets.id;
 	if (!targets)
 	{
 		message.channel.send("who u givin golden stars to");
 	}
-	else if (!parseInt(args[2]) || parseInt(args[2]) < 1 || parseInt(args[2]) > getDolla())
+	else if (!parseInt(args[2]) || parseInt(args[2]) < 1 || parseInt(args[2]) > check)
 	{
 		message.channel.send("thats not a valid number of golden stars to give")
 		
