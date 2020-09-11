315f2339a8500bd8fdfe213a187073ccc951b321
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-11 10:35:50 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,10 +5,10 @@ if (message.member.hasPermission("KICK_MEMBERS")) {
             message.members.mentions.first().kick();
             message.channel.send(":wave: " + member.displayName + " has been kicked, what a noob lol ");
         } catch {
-            message.reply("I do not have permissions to kick people, how about you give me it? " + msg.members.mentions.first());
+            message.reply("I do not have permissions to kick people, how about you give me it? " + message.members.mentions.first());
     } 
     }
     else {
-        message.reply("bruh you dont event have permission to kick people, stop trying smh " + msg.members.mentions.first());
+        message.reply("bruh you dont event have permission to kick people, stop trying smh " + message.members.mentions.first());
 }
 }
