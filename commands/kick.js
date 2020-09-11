3bcc3805780695e58c65d8c688600abf2795ae51
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-11 11:22:41 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,7 @@
 //Usage: `kick`, kicks a player if you have the perms to kick that player
 let target =  message.mentions.members.first();
-if (message.member.hasPermission("KICK_MEMBERS")) {
+let boolean = message.member.hasPermission("KICK_MEMBERS");
+if (boolean) {
     if (target) {
         try {
              target.kick();
@@ -9,7 +10,8 @@ if (message.member.hasPermission("KICK_MEMBERS")) {
             message.reply("I do not have permissions to kick people, how about you give me it? " +  target);
     } 
     }
-    else {
+    else if (!boolean) {
         message.reply("bruh you dont event have permission to kick people, stop trying smh " + target);
 }
+    else {message.channel.send("Cannot kick "+target.displayName);}
 }
