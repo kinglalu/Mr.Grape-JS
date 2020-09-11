99a8d7a3a7a337ad848d090cb2d54137141b26f3
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-11 10:35:23 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,14 +1,14 @@
 //Usage: `kick`, kicks a player if you have the perms to kick that player
-if (msg.member.hasPermission("KICK_MEMBERS")) {
-    if (msg.members.mentions.first()) {
+if (message.member.hasPermission("KICK_MEMBERS")) {
+    if (message.members.mentions.first()) {
         try {
-            msg.members.mentions.first().kick();
-            msg.channel.send(":wave: " + member.displayName + " has been kicked, what a noob lol ");
+            message.members.mentions.first().kick();
+            message.channel.send(":wave: " + member.displayName + " has been kicked, what a noob lol ");
         } catch {
-            msg.reply("I do not have permissions to kick people, how about you give me it? " + msg.members.mentions.first());
+            message.reply("I do not have permissions to kick people, how about you give me it? " + msg.members.mentions.first());
     } 
     }
     else {
-        msg.reply("bruh you dont event have permission to kick people, stop trying smh " + msg.members.mentions.first());
+        message.reply("bruh you dont event have permission to kick people, stop trying smh " + msg.members.mentions.first());
 }
 }
