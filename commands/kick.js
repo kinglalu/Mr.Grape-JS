9a56477b8f0b07199c37947fdf931575cd794e18
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-09-10 22:33:30 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,10 @@
 //Usage: `kick`, kicks a player if you have the perms
-  let member = message.mentions.members.first();
-    member.kick().then((member) => {
-        message.channel.send(`:wave: ${member.displayName} has been kicked, bye noob`);
-    }).catch(() => {
-        if (!message.member.hasPermission(['KICK_MEMBERS'])) {
-            message.reply("You cannot kick members");
-        } else if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS', 'ADMINISTRATOR'])) {
-            message.reply("You cannont kick this member");
-        }
-    })
+if (msg.member.hasPermission("KICK_MEMBERS") {
+    if (msg.members.mentions.first()) {
+        try {
+            msg.members.mentions.first().kick();
+        } catch {
+            msg.reply("I do not have permissions to kick people, how about you give me it? " + msg.members.mentions.first());
+    }else {
+        msg.reply("bruh you dont event have permission to kick people, stop trying smh " + msg.members.mentions.first());
 }
