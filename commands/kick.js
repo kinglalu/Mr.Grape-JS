1f78152b83d71598f9f2dbd1bedef684d30b3de2
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-09-10 22:30:08 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,12 @@
 //Usage: `kick`, kicks a player if you have the perms
-let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if (!buser) return message.channel.send("Please provide a user to kick")
-let reason = args.join(" ").slice(22)
-if (!reason) return message.channel.send("Please provide a reason")
-if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You cant kick people stop trying smh")
-if (buser.hasPermission("KICK_MEMBERS")) return message.channel.send("kicked")
-let embed = new Discord.RichEmbed()
-buser.ban()
-.setAuthor("Kick")
-.setDescription(`The user ${buser} has kicked ban for: ${reason}\n by the admin ${message.author.tag}`)
-bot.channels.get("an channel id").send(embed);
+  let member = message.mentions.members.first();
+    member.kick().then((member) => {
+        message.channel.send(`:wave: ${member.displayName} has been kicked, bye noob`);
+    }).catch(() => {
+        if (!message.member.hasPermission(['KICK_MEMBERS'])) {
+            message.reply("You cannot kick members");
+        } else if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS', 'ADMINISTRATOR'])) {
+            message.reply("You cannont kick this member");
+        }
+    })
+}
