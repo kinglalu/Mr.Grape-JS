968d1211e18382f16cb977da430ebffe0aaa2d86
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-14 09:18:52 -0400

    Update ttest.js

--- commands/ttest.js
+++ commands/ttest.js
@@ -3,7 +3,7 @@ async function test(param) {
 let git = await users.get(message.author.id);
 if (param > git) {
  message.channel.send('noice')
-  return true;
+  return message.channel.send('ok this part work, but does the next part work');
 }
 return false;
   }
