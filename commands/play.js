f69d790655ca52132f294ba9e23327e7324752fc
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-16 10:16:53 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,3 +1,12 @@
+ const dispatcher = serverQueue.connection
+    	.play(ytdl(song.url))
+    	.on("finish", () => {
+      	serverQueue.songs.shift();
+      	play(guild, serverQueue.songs[0]);
+    	})
+    	.on("error", error => console.error(error));
+  	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  	serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 function play(guild, song) {
   const serverQueue = queue.get(guild.id);
   if (!song) {
