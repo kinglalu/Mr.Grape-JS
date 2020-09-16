b2762ffc93fff00b6dcbd7f27631d6485cb9bf66
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-09-16 15:19:40 -0500

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,1 +1,20 @@
 
+
+const shopEmbed =  Discord.MessageEmbed()
+	.setColor('dd2de0')
+	.setTitle('The Shop')
+	.setDescription('buy some swick items here')
+	.setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+	.addFields(
+		{ name: 'fan', value: 'Some value here' },
+		{ name: 'orangedetector', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.setTimestamp()
+	.setFooter('Grape Marketplaces');
+  	  
+  message.channel.send(shopEmbed);
+				
+}
+
