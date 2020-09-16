27e831da98cd893e15f722266c363731e80945de
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-09-16 15:27:14 -0500

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,19 +1,14 @@
-//Usage: `shop`, purchase cool items to help gain more stars
-const shopEmbed =  Discord.MessageEmbed()
-	.setColor('dd2de0')
-	.setTitle('The Shop')
-	.setDescription('buy some swick items here')
-	.setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-	.addFields(
-		{ name: 'fan', value: 'Some value here' },
-		{ name: 'orangedetector', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.setTimestamp()
-	.setFooter('Grape Marketplaces');
-  	  
-  message.channel.send(shopEmbed);
-				
+//Usage: `shop`, purchase cool items to gain more stars
 
+const usersoloEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('The Shop')
+					.addFields(
+						{ name: 'fan:', value:'test'},
+						{ name: 'Orangedetector', value: 'test'},	
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Marketplaces');
 
+				message.channel.send(usersoloEmbed); 
