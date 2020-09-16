1b8daf411545b50407e545ca36696c1dae4c23ad
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-16 16:47:35 -0400

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -3,8 +3,12 @@ const usersoloEmbed = new Discord.MessageEmbed()
 					.setColor('#dd2de0')
 					.setTitle('The Shop')
 					.addFields(
-						{ name: 'fan:', value:'test'},
-						{ name: 'Orangedetector', value: 'test'},	
+						{ name: 'fan:', value:'reduces cooldowns of some commands\ncost: 100:star:'},
+						{ name: 'orangedetector', value: 'increases the chance you find an orange in the orange job\ncost: 100:star:'},	
+						{ name: 'mangodetector', value: 'increases the chance you find a mango in the mango job\ncost: 50:star:'},
+						{ name: 'carrotdetector', value: 'increases the chance you find a carrot in the carrot job\ncost: 50:star:'},
+						{ name: 'starmagnet', value: 'increases the chance you find :star:s\ncost: 100:star:'},
+						{ name: 'starmill', value: 'generates you :star:s passively at a rate of 1:star: per 10m\ncost: 400:star:'},
 					)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
