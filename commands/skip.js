11b80ce4a7f8b54bd2da1604193a4a74b1a0254f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-16 09:57:04 -0400

    Create skip.js

---
+++ commands/skip.js
@@ -0,0 +1,10 @@
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+skip(message, serverQueue);
