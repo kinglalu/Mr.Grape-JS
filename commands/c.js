2d02ac52c9e7d7e7a707c0b5fc820b22af9f421d
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-16 17:16:22 -0400

    Create c.js

---
+++ commands/c.js
@@ -0,0 +1,14 @@
+async function chill(command, duration) {
+let durationSeconds = duration * 1000;
+await keyv.set(command, message.author.id, durationSeconds);
+}
+
+chill(c, 15);
+
+async function check () {
+let chek = await keyv.get(command, message.author.id);
+if (check === undefined) {message.channel.send('Cooldown complete!');
+else {message.channel.send('Cooldown in progress bro');}
+}
+
+check();
