e55ad4ddf76712174f0b4f94606c247eed25e28c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-20 18:44:59 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,41 +20,17 @@ if (!cooldown)
 {
 	cooldowns.push(cmd + message.author.id);
 	cooldowns.push("c5");
-
-	let commands = fs.readdirSync("./commands");
-	for(let i = 0; i < commands.length; i++)
-	{
-		let info = fs.readFileSync(`./commands/${commands[i]}`).toString().split("\n")[0]
-		commands[i] = `\`${config.prefix}${commands[i].slice(0, commands[i].length - 3)}\`: ${info.slice(2, info.length)}`;
-	}
-	
-	const balsoloEmbed = new Discord.MessageEmbed()
+	const balnoEmbed = new Discord.MessageEmbed()
 					.setColor('#dd2de0')
 					.setTitle('Help')
 					.addFields(
-						{ name: 'Commands', value: commands[0] },
-						{ name: 'Commands', value: commands[1] },
-						{ name: 'Commands', value: commands[2] },
-						{ name: 'Commands', value: commands[3] },
-						{ name: 'Commands', value: commands[4] },
-						{ name: 'Commands', value: commands[5] },
-						{ name: 'Commands', value: commands[6] },
-						{ name: 'Commands', value: commands[7] },
-						{ name: 'Commands', value: commands[8] },
-						{ name: 'Commands', value: commands[9] },
-						{ name: 'Commands', value: commands[10] },
-						{ name: 'Commands', value: commands[11] },
-						{ name: 'Commands', value: commands[12] },
-						{ name: 'Commands', value: commands[13] },
-						{ name: 'Commands', value: commands[14] }
-						
-						
-						
+						{ name: 'Help', value: 'Go to: http://mr-grape.ck.ax for help!' },
 					)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
-					.setFooter('Grape Enterprises');
+					.setFooter('Grape Help')
+					
 
-				message.channel.send(balsoloEmbed);
-				
+				message.channel.send(balnoEmbed);
+	
 }
