96f9172426ddd10a94b35c09387008ce1c1c1d76
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-24 09:47:34 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,17 +20,77 @@ if (!cooldown)
 {
 	cooldowns.push(cmd + message.author.id);
 	cooldowns.push("c5");
-	const help = new Discord.MessageEmbed()
+	let helpArg = args[0];
+	if (helpArg === undefined) {
+		const balnoEmbed = new Discord.MessageEmbed()
 					.setColor('#dd2de0')
-					.setTitle('Help')
-					.addFields(
-						{ name: 'Help', value: 'Go to: http://mr-grape.ck.ax for help!' },
-					)
+					.setTitle('bro chill out and wait a bit')
+					.addFields (
+						{name: 'Help' , value: 'For more help in a specific area, do help <category>, and for info, do ${config.prefix}help'},
+						{name: 'Moni' , value: 'Help for stuff that get you stars'},
+						{name: 'Utility' , value: 'Help for stuff that get you stars'}
+						)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
-					.setFooter('Grape Help')
 					
 
-				message.channel.send(help);
-	
+				message.channel.send(balnoEmbed);
+	}
+	else if (helpArg === 'moni') {
+		
+		const balnoEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('bro chill out and wait a bit')
+					.addFields (
+						{name: 'Bal' , value: 'Check your $$$. To check the balance of another user, do ${config.prefix}bal <usermention>.'},
+						{name: 'Daily' , value: 'Get a daily amount of stars.'},
+						{name: 'Dig' , value: 'Dig to get more stars!'},
+						{name: 'Gamble' , value: 'Gamble the stars you have to get double (or nothing). 50/50 chance.'},
+						{name: 'Give' , value: 'Feeling Charitable? Give money to another user! ${config.prefix}give <user> <amount>.'},
+						{name: 'Shop' , value: 'Display the shop!'},
+						{name: 'Steal' , value: 'Steal from the big grape himself! There is a chance of losing some of your stars.'},
+						{name: 'Work' , value: 'Do some honest work to get stars! There is a chance of getting no stars (you do not lose any).'}
+						)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					
+
+				message.channel.send(balnoEmbed);	
+		
+	}
+	else if (helpArg === 'utility') {
+		
+		const balnoEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('bro chill out and wait a bit')
+					.addFields (
+						{name: 'Info' , value: 'Get some info about the bot.'},
+						{name: 'Invite' , value: 'Get the invite link for Mr. Grape!'},
+						{name: 'Kick' , value: 'Kick that noob from your server!'},
+						{name: 'Server' , value: 'Get some basic info about your server.'},
+						{name: 'Userinfo' , value: 'Get some basic info about your user id.'},
+						)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					
+
+				message.channel.send(balnoEmbed);	
+		
+	}
+	else if (helpArg === 'fun') {
+		
+		const balnoEmbed = new Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('bro chill out and wait a bit')
+					.addFields (
+						{name: '8ball' , value: 'Ask Mr. Grape what the future holds in store for you! Use like this: ${config.prefix}8ball <question>'},
+						)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					
+
+				message.channel.send(balnoEmbed);	
+		
+	}
+	else {message.channel.send('Invalid help argument dummy')}
 }
