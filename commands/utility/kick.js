d50cec30b72bd4428cdf8fb745838e3bcd45a6ba
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 20:56:59 -0400

    Update kick.js

--- commands/utility/kick.js
+++ commands/utility/kick.js
@@ -5,13 +5,13 @@ module.exports = {
     cooldown: 0,
     execute(message, args, d) {
         let rawTarget = message.mentions.members.first;
-        let target = message.guild.member(target);
+        let target = message.guild.member(rawTarget);
         let boolean = message.member.hasPermission("KICK_MEMBERS");
         if (boolean) {
             if (target) {
                     target.kick();
                     message.channel.send(":wave: " + target.displayName + " has been kicked, what a noob lol ");
-               
+       
             }
         } else if (!boolean) {
             message.reply("bruh you dont even have permission to kick people, stop trying smh ");
