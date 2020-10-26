57faac51d1397f0b82a61468b22dcc7377d6c050
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 21:01:28 -0400

    Update kick.js

--- commands/utility/kick.js
+++ commands/utility/kick.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'kick',
-    description: 'fortune telling is cool',
+    description: 'kick ppl',
     cooldown: 0,
     execute(message, args, d) {
         let rawTarget = message.mentions.members.first();
@@ -9,14 +9,18 @@ module.exports = {
         let boolean = message.member.hasPermission("KICK_MEMBERS");
         if (boolean) {
             if (target) {
+                try {
                     target.kick();
                     message.channel.send(":wave: " + target.displayName + " has been kicked, what a noob lol ");
-       
+                }
+                catch {
+                    message.channel.send("I don't got permissions (or high enough role) to kick ppl. How about ya give me it?")
+                }
             }
         } else if (!boolean) {
             message.reply("bruh you dont even have permission to kick people, stop trying smh ");
         } else {
-            message.channel.send("Cannot kick " + target.displayName);
+            message.channel.send("Cannot kick " + target.displayName + " maybe use a valid mention?");
         }
 
     }
