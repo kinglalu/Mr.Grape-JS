829f00e051bbe56798e60f8aadf65d9c4dff9d4c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 20:58:43 -0400

    Update kick.js

--- commands/utility/kick.js
+++ commands/utility/kick.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'fortune telling is cool',
     cooldown: 0,
     execute(message, args, d) {
-        let rawTarget = message.mentions.members.first;
+        let rawTarget = message.mentions.members.first();
         let target = message.guild.member(rawTarget);
         let boolean = message.member.hasPermission("KICK_MEMBERS");
         if (boolean) {
