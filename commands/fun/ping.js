b6da1995eafadbd4ad5385076c89cb9eca098488
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-10-27 09:22:10 -0500

    Create ping.js

---
+++ commands/fun/ping.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'ping',
+	aliases: ['pinginfo'],
+	description: 'basic ping pong command, see the ping',
+	cooldown: 2,
+	execute(message, args, d) {
+  var ping = m.createdTimestamp - message.createdTimestamp;
+	const pingsoloEmbed = new d.Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle('Ping')
+					.addFields(
+						{ name: 'Your ping is:', value: `${{ping}`},
+					)
+			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+					.setTimestamp()
+					.setFooter('Grape Databases');
+
+				message.channel.send(pingsoloEmbed);
+	}
+};
