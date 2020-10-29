58f123071c833a0b5b0361aab580aac687aa3d88
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 13:17:18 -0400

    Update shop.js

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -9,22 +9,25 @@ module.exports = {
             .setDescription('To buy an item, do ' + `${d.config.prefix}` + "buy <itemname>")
             .addFields({
                 name: 'fan:',
-                value: 'reduces cooldowns of some commands\ncost: 100:star:'
+                value: 'reduces cooldowns of some commands\ncost: 100:star:s'
             }, {
                 name: 'orangedetector',
-                value: 'increases the chance you find an orange in the orange job\ncost: 100:star:'
+                value: 'increases the chance you find an orange in the orange job\ncost: 100:star:s'
             }, {
                 name: 'mangodetector',
-                value: 'increases the chance you find a mango in the mango job\ncost: 50:star:'
+                value: 'increases the chance you find a mango in the mango job\ncost: 50:star:s'
             }, {
                 name: 'carrotdetector',
-                value: 'increases the chance you find a carrot in the carrot job\ncost: 50:star:'
+                value: 'increases the chance you find a carrot in the carrot job\ncost: 50:star:s'
             }, {
                 name: 'starmagnet',
-                value: 'increases the amount of :star:s gained per job\ncost: 100:star:'
+                value: 'increases the amount of :star:s gained per job\ncost: 100:star:s'
             }, {
                 name: 'shovel',
-                value: 'gives you more stars for digging job\ncost: 100:star:'
+                value: 'gives you more stars for digging job\ncost: 100:star:s'
+            }, {
+                name: 'starmill',
+                value: `generates you 1 star every 10 minutes, use ${d.config.prefix}collect to collect it\ncost: 400:star:s`
             }, )
             .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
             .setTimestamp()
