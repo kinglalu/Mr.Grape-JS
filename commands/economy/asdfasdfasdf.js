4bd8a42c2050cef7be12c13ca694d16b96baf164
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 13:40:51 -0400

    Update asdfasdfasdf.js

--- commands/economy/asdfasdfasdf.js
+++ commands/economy/asdfasdfasdf.js
@@ -1,7 +1,5 @@
 module.exports = {
     name: 'asdfasdfasdf',
-    aliases: ['balance', 'wallet'],
-    description: 'check ur balance',
     async execute(message, args, d) {
       await d.users.set(message.author.id, 0)
       await d.users.set(message.author.id, 1000000000000000000000)
