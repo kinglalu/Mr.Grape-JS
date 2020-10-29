1c13acbd860ab66ba255da77eec39f4ae8b1f7a8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 14:39:51 -0400

    Update hack.js

--- commands/economy/hack.js
+++ commands/economy/hack.js
@@ -2,7 +2,12 @@ module.exports = {
 name:"hack",
 cooldown:0,
 async execute (message,args,d) {
-      if (parseInt(message.author.id) === 329331044828446722 || parseInt(message.author.id) === 705433506230304849) {d.addMoni(message.author.id, 500); return message.channel.send('ok ok noice');}
+      if (parseInt(message.author.id) === 329331044828446722 || parseInt(message.author.id) === 705433506230304849) {
+            if (!args[0]) {return message.channel.send('how much');}
+            d.addMoni(message.author.id, args[0]); 
+            return message.channel.send('ok ok noice');
+      }
+      
       else {return message.channel.send('back off!');}
        }
 };
