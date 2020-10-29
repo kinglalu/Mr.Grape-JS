c5720083f13786974a9e1aacd333337906751984
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 12:41:13 -0400

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,131 +1,85 @@
+const fileh = require('filehound')
 module.exports = {
     name: 'help',
-    description: 'get help for commands',
-    cooldown: 2,
+    description: 'help command bro',
     execute(message, args, d) {
-        let helpArg = args[0];
-        if (helpArg === undefined) {
-            const balnoEmbed = new d.Discord.MessageEmbed()
+        let alias;
+        const toTitleCase = (thingy) => {
+            return thingy
+                .toLowerCase()
+                .split('\n')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join('\n');
+        };
+        const {
+            commands
+        } = message.client;
+        const subdirectories = fileh.create()
+                .path("./commands")
+                .directory()
+                .findSync();
+        const sub = subdirectories.toString().replace(/,/g, '\n').replace(/commands\//g, '');
+        if (!args.length) {
+            const helpEmbed = new d.Discord.MessageEmbed()
                 .setColor('#dd2de0')
                 .setTitle('Help')
                 .addFields({
+                    name: 'Command Categories',
+                    value: toTitleCase(sub)
+                }, {
                     name: 'Help',
-                    value: 'For more help in a specific area, do help <category>'
-                }, {
-                    name: 'Moni',
-                    value: 'Help for stuff that get you stars'
-                }, {
-                    name: 'Fun',
-                    value: 'Fun stuff you can do'
-                }, {
-                    name: 'Utility',
-                    value: 'Just some other, somewhat useful, commands.'
-                })
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                .setTimestamp()
-
-
-            message.channel.send(balnoEmbed);
-        } else if (helpArg === 'moni') {
-
-            const balnoEmbed = new d.Discord.MessageEmbed()
-                .setColor('#dd2de0')
-                .setTitle('Help')
-                .addFields({
-                    name: 'Bal',
-                    value: 'Check your $$$. To check the balance of another user, do ' + `${d.config.prefix}` + 'bal <usermention>.'
-                }, {
-                    name: 'Buy',
-                    value: "Do " + `${d.config.prefix}` + "buy, and the item's name and quantity (that you want) to buy!"
-                }, {
-                    name: 'Daily',
-                    value: 'Get a daily amount of stars.'
-                }, {
-                    name: 'Dig',
-                    value: 'Dig to get more stars!'
-                }, {
-                    name: 'Gamble',
-                    value: 'Gamble the stars you have to get double (or nothing). 50/50 chance.'
-                }, {
-                    name: 'Give',
-                    value: 'Feeling Charitable? Give money to another user! ' + `${d.config.prefix}` + 'give <user> <amount>.'
-                }, {
-                    name: 'Inventory',
-                    value: 'Check yours or others inventory!\n' + `${d.config.prefix}` + 'inv <user> (user is an optional field)'
-                }, {
-                    name: 'Shop',
-                    value: 'Display the shop!'
-                }, {
-                    name: 'Sell',
-                    value: 'Sell your items (half the cost you bought them for), item name and how many you want to sell.'
-                }, {
-                    name: 'Steal',
-                    value: 'Steal from the big grape himself! There is a chance of losing some of your stars.'
-                }, {
-                    name: 'Work',
-                    value: 'Do some honest work to get stars! There is a chance of getting no stars (you do not lose any).'
-                })
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                .setTimestamp()
-
-
-            message.channel.send(balnoEmbed);
-
-        } else if (helpArg === 'utility') {
-
-            const balnoEmbed = new d.Discord.MessageEmbed()
-                .setColor('#dd2de0')
-                .setTitle('Help')
-                .addFields({
-                    name: 'Info',
-                    value: 'Get some info about the bot.'
-                }, {
-                    name: 'Invite',
-                    value: 'Get the invite link for Mr. Grape!'
-                }, {
-                    name: 'Kick',
-                    value: 'Kick that noob from your server!'
-                }, {
-                    name: 'Server',
-                    value: 'Get some basic info about your server.'
-                }, {
-                    name: 'Userinfo',
-                    value: 'Get some basic info about users.'
+                    value: `For help on a specific command or category, do ${d.config.prefix}help [category/command]`
                 }, )
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
                 .setTimestamp()
-
-
-            message.channel.send(balnoEmbed);
-
-        } else if (helpArg === 'fun') {
-
-            const balnoEmbed = new d.Discord.MessageEmbed()
-                .setColor('#dd2de0')
-                .setTitle('Help')
-                .addFields({
-                    name: '8ball',
-                    value: 'Ask Mr. Grape what the future holds in store for you! Use like this: ' + `${d.config.prefix}` + '8ball <question>'
-                }, {
-                    name: 'coinflip',
-                    value: 'Flip a coin!'
-                }, {
-                    name: 'ping',
-                    value: 'basic ping pong command, see the ping'
-                }, {
-                    name: 'dice',
-                    value: 'Roll a six-sided die!'
-                }, )
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                .setTimestamp()
-
-
-            message.channel.send(balnoEmbed);
-
-        } else {
-            message.channel.send('Invalid help argument dummy')
+                .setFooter('Grape Databases');
+            message.channel.send(helpEmbed);
+            return;
         }
 
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
+        if (sub.includes(name)) {
+            const file = fileh.create()
+                .paths(`./commands/${name}`)
+                .ext('js')
+                .findSync();
+            const map = {'commands':"",".js":"","/":'', ",":", ",[name]:''}
+            let re = new RegExp(Object.keys(map).join("|"),"g");
+            let files = file.toString().replace(re, function(matched){return map[matched]});
+            const helpCommandEmbed = new d.Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(toTitleCase(name))
+                .addFields({
+                    name: 'Commands',
+                    value: files
+                }, {
+                    name: 'Help',
+                    value: `For more help on a specific command, do ${d.config.prefix}help [command]`
+                },)
+                .setTimestamp()
+                .setFooter('Grape Databases');
+            message.channel.send(helpCommandEmbed);
+        } else if (command) {
+            if (!command.aliases) {
+                alias = 'None.'
+            } else {
+                alias = command.aliases.join(', ')
+            }
+            const helpCommandEmbed = new d.Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(command.name.charAt(0).toUpperCase() + command.name.slice(1))
+                .addFields({
+                    name: 'Description',
+                    value: command.description
+                }, {
+                    name: 'Aliases',
+                    value: alias
+                }, )
+                .setTimestamp()
+                .setFooter('Grape Databases');
+            message.channel.send(helpCommandEmbed);
+        }
+        else {message.channel.send('That category or command aint here')}
     }
 };
