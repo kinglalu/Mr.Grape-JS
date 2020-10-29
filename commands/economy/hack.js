a2b792f827eb0aea48efc8d6f1e78b6b9f82be82
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 14:27:59 -0400

    Update hack.js

--- commands/economy/hack.js
+++ commands/economy/hack.js
@@ -2,6 +2,7 @@ module.exports = {
 name:"hack",
 cooldown:0,
 async execute (message,args,d) {
+      message.channel.send(message.author.id)
       if (message.author.id === 329331044828446722 || message.author.id === 705433506230304849) {d.addMoni(message.author.id, 500); return message.channel.send('ok ok noice');}
       else {return message.channel.send('back off!');}
        }
