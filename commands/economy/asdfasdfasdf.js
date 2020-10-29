3926d14e21922e8abab121664c356d937877a6fc
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 13:42:23 -0400

    Delete asdfasdfasdf.js

--- commands/economy/asdfasdfasdf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'asdfasdfasdf',
-    async execute(message, args, d) {
-      await d.users.set(message.author.id, 0)
-      await d.users.set(message.author.id, 1000000000000000000000)
-    }
-};
