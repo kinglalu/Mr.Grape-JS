d1473f7f2a5600a087dc894054f9a04f3d1aac43
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-30 14:49:16 -0400

    Create question.js

---
+++ commands/utility/question.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'question',
+    description: 'get many questions answered using wolfram alpha',
+    aliases: ['q'],
+    cooldown: 3,
+    async execute(message, args, d) {
+        if (!args[0]) {return message.channel.send('whaddya want me to look up?');}
+        let key = process.env.WOLFRAM;
+        let wolfapi = `https://api.wolframalpha.com/v1/result?i=${encodeURIComponent(args.join(' '))}&appid=${key}`;
+        let answer = await d.r2(wolfapi).text;
+        if (answer === 'No short answer available') {
+        let simpleWolf = `https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(args.join(' '))}&format=plaintext&output=JSON&appid=${key}&podindex=2`
+        let ans = await d.r2(simpleWolf).json;
+        if (!ans.queryresult.pods[0].subpods[0].plaintext || ans.queryresult.pods[0].subpods[0].plaintext === '(data not available)') {ans = "Can't find that."}
+        return message.channel.send(ans.queryresult.pods[0].subpods[0].plaintext);
+        }
+        message.channel.send(answer);
+    }
+};
