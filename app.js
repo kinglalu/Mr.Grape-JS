00f03ad8a412871e97287a22e875e08cd872df36
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 09:48:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -74,8 +74,8 @@ client.on('message', async message => {
 	const commandFanException = ['daily']
 	let inv = await items.get(message.author.id);
 	let haveFan;
-	if (inv === undefined || inv === null) {inv = {};}
-	if (inv.fan === undefined || inv.fan === null) {haveFan = 0}
+	if (inv === undefined || inv === null) {inv = {}; haveFan = 0}
+	else if (inv.fan === undefined || inv.fan === null) {haveFan = 0}
 	else {haveFan = inv.fan}
 	let cooldownAmount;
 	if (commandFanException.includes(command.name)) {cooldownAmount = command.cooldown * 1000}
