eb0b9cd36250c4cab590eaf422726dd459f3a16d
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 11:33:15 -0500

    Create joke.js

---
+++ commands/fun/joke.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'joke',
+    description: 'get a joke',
+    aliases: ['jk'],
+    cooldown: 3,
+    async execute(message, args, d) {
+     let jokeURL = "https://sv443.net/jokeapi/v2/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist&format=txt"
+     let joke = await d.r2.get(jokeURL).text;
+      const jk = new d.Discord.MessageEmbed()
+            .setColor('#dd2de0')
+            .setTitle('Joke')
+            .addFields({
+                name: joke,
+                value: '_'
+            })
+            .setTimestamp()
+            .setFooter('Grape Dice Club');
+        message.channel.send(jk);
+    }
+};
