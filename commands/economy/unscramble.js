a9d68a1f6acbada2bbe139cc9ede6d432af75bab
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-24 12:15:32 -0500

    Update unscramble.js

--- commands/economy/unscramble.js
+++ commands/economy/unscramble.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "unscramble",
     aliases: ['unsc'],
     description: "unscramble a word for $$$",
-    cooldown: 0,
+    cooldown: 30,
     async execute(message, args, d) {
         const fruitArray = [
             'apple', 'apricot', 'avocado', 'banana', 'berry', 'blackberry', 'orange', 'blueberry', 'boysenberry', 'breadfruit', 'cantaloupe', 'cherry', 'citron', 'citrus', 'coconut', 'crabapple', 'cranberry', 'currant', 'date', 'durian', 'elderberry', 'fig', 'grape', 'grapefruit', 'guava', 'honeydew', 'jackfruit', 'kiwi', 'kumquat', 'lemon', 'lime', 'lingonberry', 'loquat', 'lychee', 'mandarin', 'mango', 'melon', 'mulberry', 'nectarine', 'papaya', 'peach', 'pear', 'persimmon', 'pineapple', 'plantain', 'plum', 'pluot', 'pomegranate', 'pomelo', 'prune', 'quince', 'raisin', 'raspberry', 'strawberry', 'tangelo', 'tangerine', 'watermelon'
