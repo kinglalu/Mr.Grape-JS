bbc82d74033a4104257b914fd89992e7ddc2c295
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-01 17:19:33 -0500

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -2,14 +2,13 @@ module.exports = {
 	name: 'stop',
 	description: 'stop playing music',
 	cooldown: 2,
-  	aliases: ['leave','disconnect','dc'],
+	aliases: ['leave', 'disconnect', 'dc'],
 	execute(message, args, d) {
-		const { channel } = message.member.voice;
+		const channel = message.member.voice;
 		if (!channel) return message.channel.send("Go to a voice channel to do stuff");
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send("There ain't any music!");
-		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.end("Music stopped.");
-		message.channel.send('Done');
+		serverQueue.voiceChannel.leave();
+		message.client.queue.delete(message.guild.id);
 	}
 };
