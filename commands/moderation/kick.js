c2a437065167fd07e9e24facde0f34e55a8a5361
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-03 13:53:13 -0800

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -6,18 +6,21 @@ module.exports = {
     execute(message, args, d) {
         let rawTarget = message.mentions.members.first();
         let target = message.guild.member(rawTarget);
-        let boolean = message.member.hasPermission("KICK_MEMBERS");
+        let boolean = message.member.hasPermission("BAN_MEMBERS");
         if (boolean) {
             if (target) {
-                if (message.author.id === rawTarget.id) {return message.channel.send('Bruh imagine kicking yourself');}
+                if (message.author.id === rawTarget.id) {
+                    return message.channel.send('Bruh imagine kicking yourself');
+                }
                 try {
                     target.kick();
                     message.channel.send(":wave: " + target.displayName + " has been kicked, what a noob lol ");
-                }
-                catch {
+                } catch {
                     message.channel.send("I don't got permissions (or high enough role) to kick ppl. How about ya give me it?")
                 }
             }
+        } else if (!target) {
+            message.channel.send('who you gonna give the boot to?');
         } else if (!boolean) {
             message.reply("bruh you dont even have permission to kick people, stop trying smh ");
         } else {
