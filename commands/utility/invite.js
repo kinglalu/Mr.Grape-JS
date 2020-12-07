c850c91438f7c217be0c517f6b003170fd106b5e
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 19:42:36 -0500

    Update invite.js

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'get invite link for the bot',
 	cooldown: 2,
 	execute(message, args, d) {
-		const invite = 'https://discord.com/oauth2/authorize?client_id=743833062265323651&scope=bot&permissions=8';
+		const invite = 'https://discord.com/oauth2/authorize?client_id=743833062265323651&scope=bot&permissions=0';
 		const inviteEmbed = new d.Discord.MessageEmbed()
 			.setColor('#dd2de0')
 			.setTitle('Invite')
