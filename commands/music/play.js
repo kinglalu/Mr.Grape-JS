99a99571451787134d6f18aab10ee396eb2f0d76
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-07 14:07:11 -0500

    Updates!

--- commands/music/play.js
+++ commands/music/play.js
@@ -91,7 +91,13 @@ module.exports = {
 
 			const play = async song => {
 				const queue = message.client.queue.get(message.guild.id);
-				if (!song) { return; }
+				if (!song) {
+					setTimeout(function () {
+						message.guild.me.voice.channel.leave();
+						message.client.queue.delete(message.guild.id);
+						return;
+					}, 60000)
+				}
 				let stream = ytdl(song.url, {
 					filter: "audioonly",
 					quality: "highestaudio"
