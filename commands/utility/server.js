5c691b94ea6de45cb5214075e2dc21000dd12e22
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-12 19:33:47 -0500

    Updates!

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -3,19 +3,20 @@ module.exports = {
 	aliases: ['serverinfo'],
 	description: 'get some basic info about your server',
 	cooldown: 2,
-	cd: "Its just ur server name and count chill",
+	cd: 'Its just ur server name and count chill',
 	execute(message, args, d) {
-	const serversoloEmbed = new d.Discord.MessageEmbed()
-					.setColor('#dd2de0')
-					.setTitle('Server Info')
-					.addFields(
-						{ name: 'Server name:', value: `${message.guild.name}`},
-						{ name: 'Members:', value: `${message.guild.memberCount}`},	
-					)
-			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-					.setTimestamp()
-					.setFooter('Grape Databases');
+		const serversoloEmbed = new d.Discord.MessageEmbed()
+			.setColor('#dd2de0')
+			.setAuthor(message.guild.name, message.guild.iconURL())
+			.addFields(
+				{ name: 'Server ID', value: message.guild.id },
+				{ name: 'Members', value: message.guild.memberCount },
+				{ name: 'Created', value: message.guild.createdAt.toString().split(' ').slice(1, 4).join(' ') }
+			)
+			.setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+			.setTimestamp()
+			.setFooter('Grape Databases');
 
-				message.channel.send(serversoloEmbed);
+		message.channel.send(serversoloEmbed);
 	}
 };
