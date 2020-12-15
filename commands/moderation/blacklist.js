a1fdd501a1031702cf131c22601ec15b854617db
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-15 13:38:46 -0500

    Update blacklist.js

--- commands/moderation/blacklist.js
+++ commands/moderation/blacklist.js
@@ -4,11 +4,16 @@ module.exports = {
     cooldown: 0,
     description: 'blacklist noobs from using me',
     async execute(message, args, d) {
+		let boolean = message.member.hasPermission("MANAGE_GUILD");
+        let myBoolean = message.guild.me.hasPermission("MANAGE_GUILD");
+		if (boolean) return message.channel.send('You do not have perms');
+		if (myBoolean) return message.channel.send('I don\'t have perms');
         let guild = d.guilds.get(message.guild.id) || {};
         let target = message.mentions.members.first();
         if (!target) return message.channel.send('Who?')
         if (!guild.bl) guild.bl = []
         guild.bl.push(target.id);
-        await guilds.set(guild);
+        await d.guilds.set(message.guild.id, guild);
+		message.channel.send('blacklisted');
     }
 }
