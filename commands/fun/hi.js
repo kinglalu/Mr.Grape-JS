400f1498350244c1945ed33cdb1a30817feb9999
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-12-16 13:58:11 -0600

    Update hi.js

--- commands/fun/hi.js
+++ commands/fun/hi.js
@@ -1,20 +1,24 @@
 module.exports = {
-    name: 'hi',
-    description: 'a way for people to greet each other',
+    name: "hi",
     aliases: ['hello'],
-    cooldown: 2,
+    description: "have good manners and greet mr grape",
+    cooldown: 30,
     cd: "who says hello that much dang",
     fan: false,
-    execute(message, args, d) {
-        const toTitleCase = (thingy) => {
-            return thingy
-                .toLowerCase()
-                .split('\n')
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join('\n');
-        };
-
-        message.channel.send('hi');
-
+    async execute(message, args, d) {
+        const fruitArray = [
+            'hi', 'hello', 'yo', 'whats poppin', 'was upp', 'greetings', 'hey'
+        ]
+        function shuffle(array) {
+            const OGarray = [...array]
+            for (let i = array.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            if (OGarray.join('') === array.join('')) { return shuffle(array); }
+            else { return array.join(''); }
+        }
+        let word = fruitArray[Math.floor(Math.random() * fruitArray.length)];
+message.channel.send(word)
     }
 };
