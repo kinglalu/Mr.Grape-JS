a343d7c216783311abc50e397fd83faa6d420e62
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-12-17 11:46:28 -0600

    Create gift.js

---
+++ commands/fun/gift.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'gift',
+    description: 'temp',
+    aliases: ['gift'],
+    cooldown: 3,
+    cd: "Haha, big funny, finishing laughing first",
+
+        message.channel.send('pls use giftbox');
+    }
+};
