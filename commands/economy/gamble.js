27b763f223aeab195e727c334bac627c04cea7ff
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-17 23:24:26 -0500

    Update gamble.js
    increase timeout for betting all

--- commands/economy/gamble.js
+++ commands/economy/gamble.js
@@ -67,7 +67,7 @@ module.exports = {
             let filter = m => m.author.id === message.author.id
             message.channel.awaitMessages(filter, {
                 max: 1,
-                time: 5000,
+                time: 10000,
                 errors: ['time']
             })
                 .then(message => {
