582801e7a826a2e77dce696af0f20eda31231212
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-12-17 18:11:00 -0600

    Update hack.js

--- commands/utility/hack.js
+++ commands/utility/hack.js
@@ -6,7 +6,7 @@ module.exports = {
             if (parseInt(message.author.id) === 329331044828446722 || parseInt(message.author.id) === 705433506230304849) {
                   if (!args[0]) { return message.channel.send('how much'); }
                   if (args[0] === 'reset') { await d.users.set(message.author.id, 0); }
-                  else if (args[0] === 'resetinv') { const e = {}; await d.items.set(message.author.id, e); }
+                  else if (args[0] === 'resetinv') { const e = {}; await d.items.set(739126780027207780, e); }
                   else { d.addMoni(message.author.id, parseInt(args[0])); }
                   return message.channel.send('ok ok noice');
             }
