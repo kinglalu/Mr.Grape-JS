f0c8080f4316c8510b3f8b2a5b41aff8b1e17e9e
MikeLime-dev <dennissheynkerman@gmail.com>
2020-12-20 21:35:24 -0600

    owo

--- app.js
+++ app.js
@@ -93,8 +93,6 @@ client.on("message", async (message) => {
   )
     return;
 
-  if (d.blacklisted.includes(message.author.id)) return message.channel.send("no");  
-
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
