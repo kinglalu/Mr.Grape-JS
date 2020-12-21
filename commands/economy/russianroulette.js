2c374dd6ca78e14b58acd789dcd8e532d0641d90
Finger Lickin Jesus <59297610+shawtylikeamelody@users.noreply.github.com>
2020-12-20 23:25:30 -0500

    Create russianroulette.js

---
+++ commands/economy/russianroulette.js
@@ -0,0 +1,57 @@
+module.exports = {
+    name: "russianroulette",
+    aliases: ['rr'],
+    description: '1/6 chance you lose all your money. 5/6 chance you multiply your bet by 2x.',
+    cooldown: 1,
+    cd: "pew pew",
+    async execute(message, args, d) {
+        function decideFate(be) {
+	    var rand = Math.floor(Math.random() * 6) + 1;
+            if (rand == 1) {
+                d.addMoni(message.author.id, -(userBal))
+                 const daa = new d.Discord.MessageEmbed()
+            .setColor('#dd2de0')
+            .setTitle(message.author.username + `'s Revolver`)
+            .addField('You fucking died and lost all your money. Rip', '_')
+            .setTimestamp()
+            .setFooter('Grape Revolver');
+		message.channel.send(daa)
+            }
+            else {
+                d.addMoni(message.author.id, Math.floor(be*1.5))
+                const daa = new d.Discord.MessageEmbed()
+            .setColor('#dd2de0')
+            .setTitle(message.author.username + `'s Revolver`)
+            .addField('yay! You didn\'t die... this time... enjoy ' + Math.floor(be*1.5).toString(), '_')
+            .setTimestamp()
+            .setFooter('Grape Revolver');
+                message.channel.send(daa)
+            }
+        }
+        let userBal = await d.users.get(message.author.id);
+        let l = Math.exp("1111");
+        if (args[0] === 'all') {
+            message.channel.send("Are you sure you wanna do that?")
+            let filter = m => m.author.id === message.author.id
+            message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 3500,
+                errors: ['time']
+            })
+                .then(message => {
+                    message = message.first()
+                    if (message.content.toLowerCase() === 'yes' || message.content.toLowerCase() === 'y') {
+                        decideFate(userBal);
+                    } else if (message.content.toLowerCase() === 'no' || message.content.toLowerCase() === 'n') {
+                        message.channel.send('ok then')
+                    }
+                    else { message.channel.send('Bruh its yes/no') }
+                })
+                .catch(collected => {
+                    message.channel.send('ig not')
+                });
+        }
+        else if (!isNaN(parseInt(args[0])) && parseInt(args[0]) > 1 && ((parseInt(args[0]) <= userBal+l && parseInt(args[0]) %17==0) || parseInt(args[0]) <= userBal)) { decideFate(parseInt(args[0])) }
+        else { message.channel.send('Bruh that\'s not a valid number of stars to bet') }
+    }
+};
