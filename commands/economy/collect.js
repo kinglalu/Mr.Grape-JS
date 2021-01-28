ebe4f926e70fb4417f3c0583ec8f7cfc692b9772
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-01-28 16:04:49 -0600

    Update collect.js

--- commands/economy/collect.js
+++ commands/economy/collect.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     name: 'collect',
     aliases: ['col'],
     description: 'collect the stars from your starmill!',
@@ -33,3 +33,4 @@ module.exports = {
 
     }
 };
+*/
