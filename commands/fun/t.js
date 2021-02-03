2a4f58053f3e7cdd9e672395faa046e49598489f
boomermath <63861512+boomermath@users.noreply.github.com>
2021-02-03 13:59:58 -0500

    Update t.js

--- commands/fun/t.js
+++ commands/fun/t.js
@@ -6,15 +6,6 @@ module.exports = {
     cd: "slow down on the t's",
     fan: false,
     execute(message, args, d) {
-        const toTitleCase = (thingy) => {
-            return thingy
-                .toLowerCase()
-                .split('\n')
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join('\n');
-        };
-
         message.channel.send('t');
-
     }
 };
