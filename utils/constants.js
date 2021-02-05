9946c79e8a75676dfe5f11fa188a1e32a8dfb8a9
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-02-05 12:48:00 -0600

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -139,7 +139,7 @@ const emoji = {
   limeinite: ":question:",
 };
 
-const blacklisted = [361135858772082688, 285428039074971648];
+const blacklisted = ["361135858772082688", "285428039074971648"];
 
 module.exports = {
   Discord: Discord,
