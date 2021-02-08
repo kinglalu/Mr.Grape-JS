6d67da743e98b6df548a3a64b320bcffdf642183
CompactCow <67137312+CompactCow@users.noreply.github.com>
2021-02-08 14:17:16 -0600

    🐄 Unban me.
    Unban me and my alts so I can test the bot.

--- utils/constants.js
+++ utils/constants.js
@@ -139,7 +139,7 @@ const emoji = {
   limeinite: ":question:",
 };
 
-const blacklisted = ["361135858772082688", "285428039074971648","698432573977919580","756629154929180804"];
+const blacklisted = [];
 
 module.exports = {
   Discord: Discord,
