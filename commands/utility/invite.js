0c8b1faee229fd611a49a4c80c0a070ce4868b44
ironapple09 <78862714+ironapple09@users.noreply.github.com>
2021-02-10 09:38:58 -0500

    Update invite.js

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -5,10 +5,16 @@ module.exports = {
 	cd: "I just gave you the link!",
 	execute(message, args, d) {
 		const invite = 'https://discord.com/oauth2/authorize?client_id=743833062265323651&scope=bot&permissions=0';
+		const sever ='https://discord.gg/ZVyzbZfUqr'
 		const inviteEmbed = new d.Discord.MessageEmbed()
 			.setColor('#dd2de0')
-			.setTitle('Invite')
+			.setTitle('invites to discord sever and bot')
 			.setURL(invite)
+			.seturl (sever)
+			.setTimestamp()
+            .setFooter('Night');
 		message.channel.send(inviteEmbed);
 	}
 };
+
+	
