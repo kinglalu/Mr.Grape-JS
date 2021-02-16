b36359540ce7c261b4080ace0eb586142ca8037b
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-02-16 12:35:21 -0600

    Update server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'get some basic info about your server',
 	cooldown: 2,
 	cd: 'Its just ur server name and count chill',
-	execute(message, args, d) {
+	async execute(message, args, d) {
 		const serversoloEmbed = new d.Discord.MessageEmbed()
 			.setColor('#dd2de0')
 			.setAuthor(message.guild.name, message.guild.iconURL())
