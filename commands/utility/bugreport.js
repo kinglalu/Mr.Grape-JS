157dad2f5784b08a1d289223173bcf4c11a63c61
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-04-16 07:39:53 -0700

    add back bug report and suggestion commands

--- commands/utility/bugreport.js
+++ commands/utility/bugreport.js
@@ -1,11 +1,14 @@
-/* module.exports = {
+ module.exports = {
     name: "bugreport",
     aliases: ['bug', 'reportbug'],
     description: 'Report an bugs for Mr Grape! Any troll submits will be deleted and may face blacklist.',
     cooldown: 120,
     cd: 'damn we really have that many bugs?',
      async execute(message, args, d){
-        
+        if (args.slice(0).join(" ").length < 15)
+        return message.reply(
+          "Your bug report is too short, to help prevent abuse, please give a better description"
+        );
         const channel = message.client.channels.cache.get('811284407134322700')
         
         const query = args.join(' ');
@@ -22,7 +25,11 @@
         .setTimestamp()
         .setFooter('Grape Reports');
         channel.send(reportEmbed);
-        message.channel.send("**Bug report has been sent!**")
+        const replyEmbed = new d.Discord.MessageEmbed()
+			.setColor('#dd2de0')
+			.setTitle('**Bug Report has been sent!**')
+            .addField('⠀', 'View your report [here](https://discord.gg/2RKPmDg2A6)');
+        message.channel.send(replyEmbed);
     }
 };
-*/
+
--- commands/utility/suggestion.js
+++ commands/utility/suggestion.js
@@ -1,11 +1,14 @@
-/* module.exports = {
+ module.exports = {
     name: "suggestion",
     aliases: ['sug', 'suggest'],
     description: 'Suggest any ideas for Mr Grape! Any troll submits will be deleted and may face blacklist.',
     cooldown: 120,
     cd: 'we appriciate the ideas, but wait a bit dude',
      async execute(message, args, d){
-        
+        if (args.slice(0).join(" ").length < 15)
+        return message.reply(
+          "Your suggestion is too short, to help prevent abuse, please give a better description"
+        );
         const channel = message.client.channels.cache.get('758005843127173221')
         
         const query = args.join(' ');
@@ -22,7 +25,11 @@
         .setTimestamp()
         .setFooter('Grape Reports');
         channel.send(suggestEmbed);
-        message.channel.send("**Suggestion has been sent!**")
+        const replyEmbed = new d.Discord.MessageEmbed()
+			.setColor('#dd2de0')
+			.setTitle('**Suggestion has been sent!**')
+            .addField('⠀', 'View your suggestion [here](https://discord.gg/2RKPmDg2A6)');
+        message.channel.send(replyEmbed);
     }
 };
-*/
+
