c28beeed35f325e833f73a561360d2c44ed14690
kinglalu <56965132+kinglalu@users.noreply.github.com>
2021-04-16 10:23:23 -0700

    fix advice

--- commands/fun/advice.js
+++ commands/fun/advice.js
@@ -5,14 +5,14 @@ module.exports = {
     cooldown: 2,
     cd: "do you really need that much advice?",
     async execute(message, args, d) {
-        const { advice } = await fetch(
+        const res = await fetch(
             "https://api.adviceslip.com/advice"
           ).then((response) => response.json());
           const Embed = new d.Discord.MessageEmbed()
             .setTitle("Advice")
             .setURL("https://api.adviceslip.com/advice")
             .setColor("#dd2de0")
-            .setDescription(advice)
+            .setDescription(res.slip.advice)
             .setFooter("Grape Advisory");
           message.channel.send(Embed);
         },
